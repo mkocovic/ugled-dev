35f3471db8148c8f97963d683746fe99b2c815e5
mkg <miljankocovic34k@gmail.com>
2025-04-07 01:17:49 +0200

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-<<<<<<< HEAD
-      domains: ['images.unsplash.com','via.placeholder.com'], 
-=======
-      domains: ['images.unsplash.com','via.placeholder.com'], // add any other external image domains here
->>>>>>> b175e7001d43fbb64383e8d7cb893b4812f6fc01
+      domains: ['images.unsplash.com', 'via.placeholder.com'], // add any other external image domains here
     },
   };
   
